3794f3d02acefc4fa46789448e9bf835eef4b383
Brendon Wong <BrendonHorse18@gmail.com>
2025-09-12 13:51:01 -0700

    add new file to gitignore

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 from flask import Flask, render_template
 
+
 app = Flask(__name__)
 
 @app.route("/")
-
 def index():
     return render_template("index.html")
+
+"""@app.route("/generate")
+def generate():"""
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
